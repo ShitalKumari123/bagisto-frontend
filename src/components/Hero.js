41f23396fd770ab2850710c0b4c3a3f09a319423
Shital Sinha <shitalsinha18@gmail.com>
2024-04-09 23:54:43 +0530

    nav and hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import '../style/Hero.css'
 function Hero() {
   return (
     <div>
       <img src="https://demo.bagisto.com/bagisto-common/storage/theme/1/4MR094BrW5hB2FkJzQLO5BYEP4oeYwwpQN1WPQTO.webp" width="100%" alt="" />
+      <div className="strip">Get UPTO 40% OFF on your 1st order SHOP NOW</div>
     </div>
   )
 }
